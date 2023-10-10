40d8e64dd0c6998e64843be635da6928d30c9d11
Nathan Williams <103213957+nwilliamsgit@users.noreply.github.com>
2023-10-10 15:44:19 -0400

    Download update (#13)
    * Download Tar Update
    
    * Version update
    
    * script tar update

--- download.sh
+++ download.sh
@@ -10,6 +10,6 @@ CONTENT=$(curl -s https://api.github.com/repos/virtru/cks-setup-script/releases/
 
 URL=$(echo "$CONTENT" | grep "tarball_url*" | cut -d : -f 2,3 | tr -d \" | tr -d ,)
 
-wget $URL -qO - | tar -xf - -C cks-setup-scripts
+wget $URL -qO - | tar -xvzf - -C cks-setup-scripts
 mv cks-setup-scripts/virtru-cks-setup-script*/{VERSION,*.sh} cks-setup-scripts
 rm -rf cks-setup-scripts/virtru-cks-setup-script*
